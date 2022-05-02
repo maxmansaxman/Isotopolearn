b5bc41082da32aace06e70bd97b1c2e4d027eede
Max Lloyd <max.k.lloyd@gmail.com>
2022-05-02 14:37:56 -0400

    Unit 1 mapped out

--- site/assets/js/main.js
+++ site/assets/js/main.js
@@ -185,6 +185,19 @@
 						$(this).removeClass('inactive');
 
 				}
-			});
+		});
+		var coll = document.getElementsByClassName("collapsible");
+		var i;
 
+		for (i = 0; i < coll.length; i++) {
+		  coll[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var content = this.nextElementSibling;
+		    if (content.style.display === "block") {
+		      content.style.display = "none";
+		    } else {
+		      content.style.display = "block";
+		    }
+		  });
+		}
 })(jQuery);
